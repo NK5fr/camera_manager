19b476ba824151387c97a4d2f77d43a5d7a48eae
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2024-05-07 17:42:10 +0200

    change

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,57 +24,67 @@ MainWindow::MainWindow(QWidget *parent)
     // Init the list widget with found cameras
     setListWidget();
 
+    // Listen to item clicked in the list widget
     connect(ui->cameraList, &QListWidget::itemClicked, this, &MainWindow::openCameraWidget);
+
+    // Listen to refresh button clicked
     connect(ui->refresh, &QPushButton::clicked, this, &MainWindow::refresh);
 
-    connect(refreshTimer, SIGNAL(timeout()), this, SLOT(refresh()));
+    // Listen to refresh time event
+    connect(refreshTimer, &QTimer::timeout, this, &MainWindow::refresh);
+
+    // Start timer
     refreshTimer->start(3000);
 }
 
 MainWindow::~MainWindow()
 {
-    camList.Clear();
-    for(FlirCamera* cam : flirCamList){
+    camList.Clear(); // Clear the cam list
+    for(FlirCamera* cam : flirCamList){ // Delete all cameras
         delete cam;
     }
-    system->ReleaseInstance();
+    system->ReleaseInstance(); // Release system
     delete ui;
 }
 
+// Open the camera window
 void MainWindow::openCameraWidget(QListWidgetItem * item)
 {
-    int idx = ui->cameraList->row(item);
-    if(flirCamList[idx]->isConnected() && !open){
-        refreshTimer->stop();
+    int idx = ui->cameraList->row(item); // Get item index
+    if(flirCamList[idx]->isConnected() && !open){ // Check if the cam is still connected and if no cam is open
+        refreshTimer->stop(); // Stop timer during camera diffusion
         open = true;
         CameraWidget *currentWidget = new CameraWidget(flirCamList[idx], nullptr);
         currentWidget->show();
-        connect(currentWidget, SIGNAL(widgetClosed()), this, SLOT(cameraWidgetClosed()));
-    }else if(!flirCamList[idx]->isConnected()){
+        connect(currentWidget, &CameraWidget::widgetClosed, this, &MainWindow::cameraWidgetClosed); // Listen to camera closing
+    }else if(!flirCamList[idx]->isConnected()){ // If the camera is disconnected then refresh
         utils::showError("Camera not found");
         refresh();
-    }else{
+    }else{ // If a cam is open
         utils::showError("A camera is already opened");
     }
 }
 
+// Catch main window closing
 void MainWindow::closeEvent(QCloseEvent *event) {
     QMainWindow::closeEvent(event);
-    QApplication::closeAllWindows();
+    QApplication::closeAllWindows(); // Close all opened window
 }
 
+// Create one item in the list for each cam
 void MainWindow::setListWidget()
 {
     ui->cameraList->clear();
     for(FlirCamera *cam : flirCamList){
         string serial = cam->getSerial();
         QListWidgetItem *item = new QListWidgetItem(QString::fromStdString(serial));
-        item->setTextAlignment(Qt::AlignCenter);
-        item->setFont(QFont ("Courier", 14));
+        item->setTextAlignment(Qt::AlignCenter); // Change text alignement to center
+        item->setFont(QFont ("Courier", 14)); // Change font
         ui->cameraList->addItem(item);
     }
 }
 
+// Clear all cam list and recreate them
 void MainWindow::refresh()
 {
     flirCamList.clear();
@@ -88,10 +98,11 @@ void MainWindow::refresh()
     setListWidget();
 }
 
+// Catch camera window closing
 void MainWindow::cameraWidgetClosed()
 {
     open = false;
-    refreshTimer->start(3000);
-    refresh();
+    refreshTimer->start(3000); // restart timer
+    refresh(); // refresh automatically
 }
 
